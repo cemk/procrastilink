fe6bc087a17e7a2c692cb20b38474fe6b993613e
Cem K <git@cemkesemen.com>
2017-05-11 11:34:54 +0300

    update first links

--- site/templates/install.php
+++ site/templates/install.php
@@ -12,21 +12,21 @@
 			$page->create('urls', 'view');
 
 			//TODAY
-			$links = ['http://some.design', 'https://theestablishment.co/hating-comic-sans-is-ableist-bc4a4de87093', 'http://cemkesemen.com'];
+			$links = ['https://workflow.is/workflows/918e6365d1d34c7abd7b4beca5951e3a', 'http://some.design', 'http://cemkesemen.com'];
 			$page->create('urls/'.date('Ymd', strtotime('-1 days')), 'urls', array(
 				    'urls' => yaml::encode($links),
 				    'date' => date('Y-m-d', strtotime('-1 days'))
 				  ));
 
 			//YESTERDAY
-			$links = ['http://www.newyorker.com/magazine/2016/12/19/how-to-be-a-stoic', 'http://chrishacken.com/starting-an-internet-service-provider/', 'https://arstechnica.com/cars/2017/02/have-you-looked-at-your-windshield-wipers-lately/'];
+			$links = ['http://www.newyorker.com/magazine/2016/12/19/how-to-be-a-stoic', 'https://theestablishment.co/hating-comic-sans-is-ableist-bc4a4de87093', 'https://arstechnica.com/cars/2017/02/have-you-looked-at-your-windshield-wipers-lately/'];
 			$page->create('urls/'.date('Ymd', strtotime('-2 days')), 'urls', array(
 				    'urls' => yaml::encode($links),
 				    'date' => date('Y-m-d', strtotime('-2 days'))
 				  ));
 
 			//THE DAY BEFORE
-			$links = ['https://www.netlify.com/blog/2016/11/17/serverless-file-uploads/', 'https://monzo.com/blog/2017/01/27/designing-product-mental-health-mind/', 'https://gowebexamples.github.io/'];
+			$links = ['https://www.netlify.com/blog/2016/11/17/serverless-file-uploads/', 'https://monzo.com/blog/2017/01/27/designing-product-mental-health-mind/', 'https://getkirby.com/'];
 			$page->create('urls/'.date('Ymd', strtotime('-3 days')), 'urls', array(
 				    'urls' => yaml::encode($links),
 				    'date' => date('Y-m-d', strtotime('-3 days'))
